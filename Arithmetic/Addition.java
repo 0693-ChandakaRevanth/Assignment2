f1b3d27a2cd60a191cbcee0d643ba0fc86efaba1
Revanth  Chandaka <Revanth.Chandaka@godigit.com>
2024-07-31 14:49:52 +0530

    Arithmetic Operators

---
+++ Arithmetic/Addition.java
@@ -0,0 +1,14 @@
+package Arithmetic;
+
+import java.util.Scanner;
+
+public class Addition {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int m=sc.nextInt();
+        System.out.println(n+m);
+        System.out.println(Math.abs(n-m));
+    }
+}
+
